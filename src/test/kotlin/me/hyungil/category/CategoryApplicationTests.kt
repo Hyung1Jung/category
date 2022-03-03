4d83ea92a42ab6705cc0d765e071f9cdaee219cd
Hyung1Jung <junghyungile@gmail.com>
2022-03-04 02:53:56 +0900

    [#1] 프로젝트 시작 및 설정 (#2)
    프로젝트 시작 및 설정
    - springBoot 2.6.4, jdk17, kotlin, h2, jpa
    - commit message template, pull request template

---
+++ src/test/kotlin/me/hyungil/category/CategoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.hyungil.category
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CategoryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
